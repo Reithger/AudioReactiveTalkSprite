bbb49e052bd87c9b58c0be0b4f20dc1bbad90b9d
Ada <39278228+Reithger@users.noreply.github.com>
2025-03-25 17:30:02 -0300

    Adjusted global status of variable in read_audio.py so that it stops freezing after a minute of use; not entirely sure why that was breaking it but oh well. Also added auto-overwrite of read_audio.py file if the version in the program doesn't match the version in the file system (so it auto-updates the read_audio.py when a new runnable is released and it needs to change, don't gotta delete it manually to reset it

--- src/main/AudioReading.java
+++ src/main/AudioReading.java
@@ -104,9 +104,10 @@ public class AudioReading {
 	
 	private void verifyPythonFileNear() {
 		File f = new File(Controller.CONFIG_FILE_PATH + "/read_audio.py");
-		if(!f.exists()) {
+		if(!f.exists() || !validateFileCorrect(f)) {
 			try {
 				ArrayList<String> contents = getTemplatePythonContents();
+				f.delete();
 				f.createNewFile();
 				RandomAccessFile raf = new RandomAccessFile(f, "rw");
 				for(String s : contents) {
@@ -120,6 +121,38 @@ public class AudioReading {
 		}
 	}
 	
+	private boolean validateFileCorrect(File f) {
+		System.out.println("Validating read_audio.py file correctness");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(f);
+			ArrayList<String> compare = new ArrayList<String>();
+			while(sc.hasNextLine()) {
+				compare.add(sc.nextLine());
+			}
+			sc.close();
+			ArrayList<String> correct = getTemplatePythonContents();
+			if(compare.size() != correct.size()) {
+				System.out.println("read_audio.py file not validated, rewrite");
+				return false;
+			}
+			for(int i = 0; i < compare.size(); i++) {
+				if(!compare.get(i).equals(correct.get(i))) {
+					System.out.println("read_audio.py file not validated, rewrite");
+					return false;
+				}
+			}
+			System.out.println("read_audio.py file validated, safe to use");
+			return true;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("read_audio.py file not validated, rewrite; error occured");
+			return false;
+		}
+		
+	}
+	
 	private ArrayList<String> getTemplatePythonContents() {
 		InputStream is = null;
 		Scanner sc;
@@ -131,7 +164,6 @@ public class AudioReading {
 			try {
 				File f;
 				f = new File(Controller.CONFIG_FILE_PATH + "read_audio.txt");
-				System.out.println(f.getAbsolutePath());
 				sc = new Scanner(f);
 			}
 			catch(Exception e1) {
